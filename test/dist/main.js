0e7eed8c8891a2373b627780d8ad1e9e8ccbae31
bobihuang <bobihuang@tencent.com>
2024-11-20 16:48:26 +0800

    chore: bump

--- test/dist/main.js
+++ test/dist/main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";var r={},o={};function e(t){var n=o[t];if(void 0!==n)return n.exports;var s=o[t]={exports:{}};return r[t](s,s.exports,e),s.exports}e.rv=function(){return"1.0.5"},e.ruid="bundler=rspack@1.0.5",console.log("Hello, world!")})();
+(()=>{"use strict";var r={},o={};function e(t){var n=o[t];if(void 0!==n)return n.exports;var s=o[t]={exports:{}};return r[t](s,s.exports,e),s.exports}e.rv=function(){return"1.1.2"},e.ruid="bundler=rspack@1.1.2",console.log("Hello, world!")})();
